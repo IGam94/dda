4b0a40a6f7d3c8fa6e2a4093ae9baf0f458d0fdf
IGam94 <45707242+IGam94@users.noreply.github.com>
2020-04-25 08:26:36 +0900

    add

---
+++ precache-manifest.ab081b2a6d60ac20468f8fba89805e0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd5a3f28d1dd2ed9fec8bbd8a55ecdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16abe7ad3819f5962258",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "4a11522315a55044c238",
+    "url": "/static/css/main.d44923e7.chunk.css"
+  },
+  {
+    "revision": "16abe7ad3819f5962258",
+    "url": "/static/js/2.99fecd4b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.99fecd4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a11522315a55044c238",
+    "url": "/static/js/main.de4999f8.chunk.js"
+  },
+  {
+    "revision": "820dd4a340c9ae8bf73d",
+    "url": "/static/js/runtime-main.a8c1f9ad.js"
+  }
+]);
